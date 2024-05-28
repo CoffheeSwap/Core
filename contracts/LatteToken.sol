98b7d5ea86081e85ac069419ba2d74573837269f
Lamont V <lamontsprin@aol.com>
2024-05-28 07:25:49 -0400

    Update and rename MockToken.sol to LatteToken.sol

--- contracts/LatteToken.sol
+++ contracts/LatteToken.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.19;
 import { FHE, euint16 } from "@fhenixprotocol/contracts/FHE.sol";
 import { FHERC20 } from "./FHERC20.sol";
 
-contract MockToken is FHERC20 {
+contract LatteToken is FHERC20 {
     uint16 amount;
 
     constructor(string memory name, string memory symbol, uint16 dripAmount) FHERC20(name, symbol){
